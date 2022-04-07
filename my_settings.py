0235bcdab108250609e9cdccd94d30dd9c6ddce5
전예찬 맥북에어 <yechan@jeon-yechan-ui-MacBookAir.local>
2022-04-07 18:18:15 +0900

    Add: django project setting

---
+++ my_settings.py
@@ -0,0 +1,12 @@
+SECRET_KEY = 'django-insecure-f4*1lp7!4h$kfb*v=!b273x0$+#tmv=91%2#9&kn1_s#ydd1p2'
+
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'settingtest',
+        'USER': 'root',
+        'PASSWORD': '0421',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
